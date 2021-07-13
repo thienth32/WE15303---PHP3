f5a7834d9dd00fc2dd0820b645f08852115b0707
thienth32 <64114903+thienth32@users.noreply.github.com>
2021-07-13 13:45:19 +0700

    update routing

---
+++ routes/admin.php
@@ -0,0 +1,9 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+Route::get('dashboard', function(){
+    return "Admin Dashboard";
+});
+
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,29 +18,19 @@ use App\Models\Category;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $products = Product::all();
+    return view('welcome', compact('products'));
+})->name('homepage');
+
+Route::get('users/{name}/{age?}', function($name, $age = 1){
+    return route('demo_route', ['id' => 100]);
 });
 
-Route::get('users', [HomeController::class, 'index']);
-Route::get('users/detail/{id}', [HomeController::class, 'detail']);
+Route::get('dang-nhap', function(){
+    return "trang đăng nhập";
+})->name('login');
 
-
-// truyền dữ liệu từ đường dẫn a => đường dẫn b
-Route::get('info', [HomeController::class, 'infoForm']);
-Route::get('luu-info', [HomeController::class, 'saveInfo'])->name('save.info');
-Route::get('test-connection', function(){
-    $users = User::all();
-    $cates = Category::all();
-    dd($cates);
-});
-
-/**
- * trong form đc sinh ra từ url: 'info'
- * thêm các field:
- * tuổi
- * giới tính (3 thẻ input radio: value "1" - nam, "2" - nữ, "3" - khác)
- * đã kết hôn hay chưa (1 thẻ input checkbox có value = "1" - nếu tích thì là đã kết hôn rồi, 
- * nếu không tích thì là đang độc thân)
- * sau đó submit form lên đường dẫn 'luu-info' 
- * tại đây sinh ra 1 màn hình hiển thị thông tin dữ liệu của người dùng vừa gửi data ra html
- */
+Route::get('demo-name/{id}', function($id){
+    return $id;
+})->middleware(['auth'])->name('demo_route');
