a4bd01378e434846dd78161ed473f025841fe99d
thienth32 <64114903+thienth32@users.noreply.github.com>
2021-07-29 13:17:32 +0700

    Update index.blade.php

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -1,33 +1,45 @@
 @extends('admin.layouts.main')
 @section('title', 'PT15303 - Quản trị sản phẩm')
 @section('content')
-<fieldset>
-    <legend>Tìm kiếm</legend>
-    <form action="" method="get">
-        <label for="">Tên sp:</label>
-        <input type="text" name="keyword">
-        <br>
-        <label for="">Danh mục:</label>
-        <select name="cate_id" id="">
-            <option value="">Tất cả</option>
-            @foreach($cates as $c)
-            <option value="{{$c->id}}">{{$c->name}}</option>
-            @endforeach
-        </select>
-        <br>
-        <label for="">Sắp xếp theo</label>
-        <select name="order_by" id="">
-            <option value="">Mặc định</option>
-            @foreach(config('common.product_order_by') as $k => $val)
-            <option value="{{$k}}">{{$val}}</option>
-            @endforeach
-        </select>
-        <br>
-        <button type="submit">Tìm kiếm</button>
-    </form>
-</fieldset>
 
-<table>
+<form action="" method="get">
+    <div class="row">
+        <div class="col-4">
+            <div class="form-group">
+                <label for="">Tên sp:</label>
+                <input type="text" name="keyword" class="form-control">
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="">Danh mục:</label>
+                <select name="cate_id" class="form-control">
+                    <option value="">Tất cả</option>
+                    @foreach($cates as $c)
+                    <option value="{{$c->id}}">{{$c->name}}</option>
+                    @endforeach
+                </select>    
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="form-group">
+                <label for="">Sắp xếp theo</label>
+                <select name="order_by" class="form-control">
+                    <option value="">Mặc định</option>
+                    @foreach(config('common.product_order_by') as $k => $val)
+                    <option value="{{$k}}">{{$val}}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="col-12 d-flex justify-content-center">
+            <button class="btn btn-sm btn-primary" type="submit">Tìm kiếm</button>
+        </div>    
+    </div>
+</form>
+<br>
+
+<table class="table table-hover">
     <thead>
         <th>STT</th>
         <th>Tên SP</th>
